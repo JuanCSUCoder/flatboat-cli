45b2eb49c186e5e60ba4ec0a10380f2150d8cc82
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-08-05 15:46:06 -0500

    Feat: Workspace Sub-commands

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, Args};
+
+#[derive(Subcommand)]
+enum WorkspaceSubcommands {
+    /// Creates a new workspace in the specified location
+    Create,
+
+    /// Lists existing workspaces
+    List,
+
+    /// Deletes an existing workspace
+    Delete
+}
+
+/// Workspace Subcommands
+#[derive(Args)]
+struct WorkspaceArgs {
+
+    /// Workspace Sub-commands
+    #[command(subcommand)]
+    subcommand: WorkspaceSubcommands,
+}
 
 /// Commands
 #[derive(Subcommand)]
 enum Commands {
-    Workspace,
+    /// Commands to create and manipulate a Dockerized ROS2 Workspace
+    Workspace(WorkspaceArgs),
+
+    /// Information about the command-line application
     Info
 }
 
 /// FlatBoat - CLI for robotics containerized docker environment
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
-struct Args {
+struct Cli {
+
+    /// Subcommand Category
     #[command(subcommand)]
     command: Commands,
 }
 
 fn main() {
-    let cli = Args::parse();
+    let _cli = Cli::parse();
     println!("Hello, world!");
 }
