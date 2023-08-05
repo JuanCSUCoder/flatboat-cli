9ef17462745215979c3d0f6e893fc17f691f21a0
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-08-05 16:05:34 -0500

    Fix

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ fn main() {
     match cli.command {
         args::Commands::Workspace(ws_args) => match ws_args.subcommand {
             args::WorkspaceSubcommands::Create { ws_name } => {
-                println!("Creating Workspace {}", ws_name);
+                println!("Creating Workspace {} ...", ws_name);
                 todo!()
             },
             args::WorkspaceSubcommands::List => {
-                println!("Available Workspaces ------------------");
+                println!("Available Workspaces");
                 todo!();
             },
             args::WorkspaceSubcommands::Delete { ws_name } => {
-                println!("Deleting Workspace {}", ws_name);
+                println!("Deleting Workspace {} ...", ws_name);
                 todo!();
             },
         },
