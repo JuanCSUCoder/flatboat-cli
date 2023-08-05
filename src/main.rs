3d8c7bbecd156b603b52960fcf683342e3580217
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-08-05 15:29:50 -0500

    CLI Base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{Parser, Subcommand};
+
+/// Commands
+#[derive(Subcommand)]
+enum Commands {
+    Workspace,
+    Info
+}
+
+/// FlatBoat - CLI for robotics containerized docker environment
+#[derive(Parser)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
 fn main() {
+    let cli = Args::parse();
     println!("Hello, world!");
 }
