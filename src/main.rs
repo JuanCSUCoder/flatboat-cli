c265f4952c53e3ab4a672e45dcfcba722ea99e7b
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-08-12 12:20:07 -0500

    Feat: Folder creation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod args;
 extern crate pretty_env_logger;
 #[macro_use] extern crate log;
 
+use std::{fs, process, path::Path};
+
 use args::Cli;
 use clap::Parser;
 use subprocess::Exec;
@@ -14,8 +16,16 @@ fn main() {
     match cli.command {
         args::Commands::Workspace(ws_args) => match ws_args.subcommand {
             args::WorkspaceSubcommands::Create { ws_name } => {
-                info!("Creating Workspace {} ...", ws_name);
-                Exec::cmd("docker").arg("info").join().unwrap();
+                info!("Creating Workspace {} ...", &ws_name);
+                let path = Path::new(&ws_name);
+                match fs::create_dir(&path) {
+                    Ok(_) => info!("Folder {} created at {:?}", &ws_name, path.canonicalize()),
+                    Err(e) => {
+                        error!("Unable to create workspace folder {} at {:?}: {}", &ws_name, path.canonicalize(), e);
+                        process::exit(1);
+                    },
+                };
+                // Exec::cmd("docker").arg("info").join().unwrap();
                 error!("Not Implemented");
                 todo!()
             },
