01d05c1e48a816175c8a67f0d5d0b810f9c08809
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-24 09:50:12 -0500

    Feat: implements cmds handles

--- src/features/cmds.rs
+++ src/features/cmds.rs
@@ -1,11 +1,27 @@
-use crate::args::{Ros2Args, ExecArgs};
+use crate::{args::{Ros2Args, ExecArgs}, toolkits::devcontainer};
 
 /// Handles ROS2 Command Arguments
 pub fn handle_ros2_cmd(ros2_args: Ros2Args) {
-    
+    if devcontainer::run_devcontainer().is_ok() {
+        if devcontainer::exec_in_shell("ros2 ".to_string() + &ros2_args.ros2_args).is_ok() {
+            debug!("Command Executed Successfully")
+        } else {
+            error!("Command Execution Failed")
+        }
+    } else {
+        error!("Failed to start the devcontainer, check if the current directory is based on a valid devcontainer template")
+    }
 }
 
 /// Handles Exec Command Arguments
 pub fn handle_exec_cmd(exec_args: ExecArgs) {
-    
+    if devcontainer::run_devcontainer().is_ok() {
+        if devcontainer::exec_in_shell(exec_args.exec_cmd).is_ok() {
+            debug!("Command Executed Successfully")
+        } else {
+            error!("Command Execution Failed")
+        }
+    } else {
+        error!("Failed to start the devcontainer, check if the current directory is based on a valid devcontainer template")
+    }
 }
--- src/toolkits/devcontainer.rs
+++ src/toolkits/devcontainer.rs
@@ -1,5 +1,12 @@
 use subprocess::{Exec, PopenError, ExitStatus};
 
+/// Checks if a valid devcontainer is already running, and starts it if its not
+pub fn run_devcontainer() -> Result<ExitStatus, PopenError> {
+    Exec::cmd("devcontainer")
+        .args(&["up", "--workspace-folder", "."])
+        .join()
+}
+
 /// Executes a Linux Command Inside of the Devcontainer
 pub fn exec_in_shell(cmd: String) -> Result<ExitStatus, PopenError> {
     Exec::cmd("devcontainer")
