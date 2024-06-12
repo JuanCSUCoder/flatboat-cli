d582d2fe3417c065a7bad7d3fea4106c1885d91f
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-06-12 17:40:30 -0500

    Feat: Simplified JSON Query command

--- src/main.rs
+++ src/main.rs
@@ -42,9 +42,9 @@ async fn run_command(cli: Cli, _project_dirs: ProjectDirs) -> ProgramResult {
 }
 
 fn output_serialized(output: &impl serde::Serialize) {
-    println!("====== RESULT =======");
-    print!("{}", toml::ser::to_string_pretty(&output).expect("TOML Serializer"));
-    println!("=====================");
+    info!("====== RESULT =======");
+    info!("{}", toml::ser::to_string_pretty(&output).expect("TOML Serializer"));
+    info!("=====================");
     println!("{}", serde_json::to_string(&output).expect("JSON Serializer"));
 }
 
