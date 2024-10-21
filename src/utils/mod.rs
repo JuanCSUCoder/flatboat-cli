0c943929f1cced0e483eaf9e28d8329c4059c349
JuanCSUCoder <jcsu2019369@gmail.com>
2024-10-21 16:28:55 -0500

    Feat: Public Private Module

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,24 @@
-pub mod manifest;
-pub mod constants;
-pub mod exec;
-pub mod package_config;
-pub mod pull;
-pub mod result;
+mod manifest_utils;
+mod constants;
+mod exec;
+mod package_config;
+mod pull_utils;
+mod result;
+
+mod public {
+  pub mod manifest {
+    pub use crate::utils::manifest_utils::Manifest;
+    pub mod result {
+      pub use crate::utils::manifest_utils::result::ManifestError;
+    }
+  }
+
+  pub mod pull {
+
+  }
+}
+
+pub use public::{
+  manifest,
+  pull,
+};
