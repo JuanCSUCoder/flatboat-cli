704fec88a2f47499f75ea2877e477646a122aa66
JuanCSUCoder <jcsu2019369@gmail.com>
2024-10-21 16:40:50 -0500

    Feat: Public private module for toolkits

--- src/toolkits/mod.rs
+++ src/toolkits/mod.rs
@@ -1,3 +1,27 @@
-pub mod devcontainer;
-pub mod oras;
-pub mod docker;
+mod devcontainer_tk;
+mod oras_tk;
+mod docker_tk;
+
+mod public {
+  pub mod devcontainer {
+    pub use crate::toolkits::devcontainer_tk::{
+      run_devcontainer,
+      exec_in_shell,
+      create_ws_files,
+    };
+  }
+
+  pub mod oras {
+    pub use crate::toolkits::oras_tk::pull_template;
+  }
+
+  pub mod docker {
+    pub use crate::toolkits::docker_tk::build_image;
+  }
+}
+
+pub use public::{
+  devcontainer,
+  oras,
+  docker,
+};
