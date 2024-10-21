595213bb5c9780a628ee200564efc5fe0a0db508
JuanCSUCoder <jcsu2019369@gmail.com>
2024-10-21 16:38:29 -0500

    Feat: Finish implementing the public private module pattern

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,9 +1,9 @@
 mod manifest_utils;
-mod constants;
-mod exec;
-mod package_config;
+mod constants_utils;
+mod exec_utils;
+mod package_config_utils;
 mod pull_utils;
-mod result;
+mod result_utils;
 
 mod public {
   pub mod manifest {
@@ -14,11 +14,34 @@ mod public {
   }
 
   pub mod pull {
+    pub use crate::utils::pull_utils::{
+      Pullable,
+      PullError,
+    };
+  }
 
+  pub mod constants {
+    pub use crate::utils::constants_utils::BASE_URL;
+  }
+
+  pub mod exec {
+    pub use crate::utils::exec_utils::wrapped_exec;
+  }
+
+  pub mod package_config {
+    pub use crate::utils::package_config_utils::PackageConfig;
+  }
+
+  pub mod result {
+    pub use crate::utils::result_utils::PackageConfigError;
   }
 }
 
 pub use public::{
   manifest,
   pull,
+  constants,
+  exec,
+  package_config,
+  result
 };
