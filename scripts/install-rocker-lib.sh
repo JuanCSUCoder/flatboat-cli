a999f19696490c1596420d47faabc28eb37ff873
JuanCSUCoder <jcsu2019369@gmail.com>
2024-11-26 11:38:18 -0500

    Feat: Adds extra files remove

--- scripts/install-rocker-lib.sh
+++ scripts/install-rocker-lib.sh
@@ -3,3 +3,8 @@
 sudo mkdir -p /usr/local/lib
 cd /usr/local/lib
 sudo git clone https://github.com/osrf/rocker.git
+
+# sudo rm /usr/local/lib/rocker/src/rocker/__init__.py
+# sudo rm /usr/local/lib/rocker/src/rocker/cli.py
+# sudo rm /usr/local/lib/rocker/src/rocker/core.py
+# sudo rm /usr/local/lib/rocker/src/rocker/em.py
