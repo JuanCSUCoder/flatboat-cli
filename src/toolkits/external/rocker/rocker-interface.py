0c7c44bf277d7b962957f02c82e3aecb34c23820
JuanCSUCoder <jcsu2019369@gmail.com>
2025-01-08 19:38:21 -0500

    Feat: Rocker Extensions Import

--- src/toolkits/external/rocker/rocker-interface.py
+++ src/toolkits/external/rocker/rocker-interface.py
@@ -1,3 +1,31 @@
+import inspect
+import rocker
+
+# Default Extensions Imports
+import rocker.extensions
+import rocker.git_extension
+import rocker.nvidia_extension
+import rocker.os_detector
+import rocker.rmw_extension
+import rocker.ssh_extension
+import rocker.volume_extension
+
+# 1. Get Default Extensions Modules
+extensions = [rocker.extensions, rocker.git_extension, rocker.nvidia_extension, rocker.rmw_extension, rocker.ssh_extension, rocker.volume_extension]
+
+
+# 2. Get Extensions Classes
+print("##### DETECTING EXTENSIONS #####")
+extension_classes = []
+for extension in extensions:
+  for name, obj in inspect.getmembers(extension):
+    if inspect.isclass(obj) and [b.__name__ for b in obj.__bases__][0] == 'RockerExtension':
+      print("Extension Class: ", name, " Inherits: ", [b.__name__ for b in obj.__bases__])
+      extension_classes.append(obj)
+    #end if
+  #end for
+#end for
+
 def generate_dockerfile():
   pass
 
