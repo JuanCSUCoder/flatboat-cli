24a6c6e0d855d81f8fba4b9cf366832f26b482e3
JuanCSUCoder <jcsu2019369@gmail.com>
2025-01-08 19:57:31 -0500

    Feat: Try Implementation of Extension Sorting

--- src/toolkits/external/rocker/rocker-interface.py
+++ src/toolkits/external/rocker/rocker-interface.py
@@ -1,4 +1,5 @@
 import inspect
+import typing
 import rocker
 
 # Default Extensions Imports
@@ -10,24 +11,85 @@ import rocker.rmw_extension
 import rocker.ssh_extension
 import rocker.volume_extension
 
+# Open Robotics Topological Extension Sorter - APACHE 2.0
+
+def sort_extensions(extensions, cli_args=""):
+  def topological_sort(source: typing.Dict[str, typing.Set[str]]):
+    """Perform a topological sort on names and dependencies and returns the sorted list of names."""
+    names = set(source.keys())
+    # prune optional dependencies if they are not present (at this point the required check has already occurred)
+    pending = [(name, dependencies.intersection(names)) for name, dependencies in source.items()]
+    emitted = []
+    while pending:
+      next_pending = []
+      next_emitted = []
+      for entry in pending:
+        name, deps = entry
+        deps.difference_update(emitted)  # remove dependencies already emitted
+        if deps:  # still has dependencies? recheck during next pass
+          next_pending.append(entry)
+        else:  # no more dependencies? time to emit
+          yield name
+          next_emitted.append(name)  # remember what was emitted for difference_update()
+        #end if
+      #end for
+      if not next_emitted:
+        raise Exception("Cyclic dependancy detected: %r" % (next_pending,))
+      #end if
+      pending = next_pending
+      emitted = next_emitted
+    #end while
+  #end def
+
+  extension_graph = {name: cls.invoke_after(cli_args) for name, cls in sorted(extensions.items())}
+  active_extension_list = [extensions[name] for name in topological_sort(extension_graph)]
+  return active_extension_list
+#end def
+
 # 1. Get Default Extensions Modules
-extensions = [rocker.extensions, rocker.git_extension, rocker.nvidia_extension, rocker.rmw_extension, rocker.ssh_extension, rocker.volume_extension]
+extension_modules = [rocker.extensions, rocker.git_extension, rocker.nvidia_extension, rocker.rmw_extension, rocker.ssh_extension, rocker.volume_extension]
 
 
 # 2. Get Extensions Classes
 print("##### DETECTING EXTENSIONS #####")
-extension_classes = []
-for extension in extensions:
-  for name, obj in inspect.getmembers(extension):
+extensions = []
+for extension_mod in extension_modules:
+  for name, obj in inspect.getmembers(extension_mod):
     if inspect.isclass(obj) and [b.__name__ for b in obj.__bases__][0] == 'RockerExtension':
       print("Extension Class: ", name, " Inherits: ", [b.__name__ for b in obj.__bases__])
-      extension_classes.append(obj)
+      extensions.append(obj)
     #end if
   #end for
 #end for
 
-def generate_dockerfile():
-  pass
+extensions = sort_extensions(extensions)
+
+print("Active Extensions: ", extensions)
+
+def generate_dockerfile(extensions, args_dict, base_image):
+    dockerfile_str = ''
+    # Preamble snippets
+    for el in extensions:
+        dockerfile_str += '# Preamble from extension [%s]\n' % el.get_name()
+        dockerfile_str += el.get_preamble(args_dict) + '\n'
+    dockerfile_str += '\nFROM %s\n' % base_image
+    # ROOT snippets
+    dockerfile_str += 'USER root\n'
+    for el in extensions:
+        dockerfile_str += '# Snippet from extension [%s]\n' % el.get_name()
+        dockerfile_str += el.get_snippet(args_dict) + '\n'
+    # Set USER if user extension activated
+    if 'user' in args_dict and args_dict['user']:
+        if 'user_override_name' in args_dict and args_dict['user_override_name']:
+            username = args_dict['user_override_name']
+        else:
+            username = get_user_name()
+        dockerfile_str += f'USER {username}\n'
+    # USER snippets
+    for el in extensions:
+        dockerfile_str += '# User Snippet from extension [%s]\n' % el.get_name()
+        dockerfile_str += el.get_user_snippet(args_dict) + '\n'
+    return dockerfile_str
 
 def generate_parameters():
   pass
