4d58611d7862d5208e403cfe31922fdfa438f78a
JuanCSUCoder <jcsu2019369@gmail.com>
2025-01-21 17:43:23 -0500

    Feat: Commented generate dockerfile

--- src/toolkits/external/rocker/rocker-interface.py
+++ src/toolkits/external/rocker/rocker-interface.py
@@ -48,50 +48,68 @@ def sort_extensions(extensions, cli_args=""):
 
 ## END Open Robotics Topological Extension Sorter - APACHE 2.0 ##
 
-# 1. Get Default Extensions Modules
-extension_modules = [rocker.extensions, rocker.git_extension, rocker.nvidia_extension, rocker.rmw_extension, rocker.ssh_extension, rocker.volume_extension]
+def get_extensions(extensions_modules: list, args_dict: dict):
+  """
+  
+  """
+  # 1. Get Default Extensions Modules
+  extension_mods = [rocker.extensions, rocker.git_extension, rocker.nvidia_extension, rocker.rmw_extension, rocker.ssh_extension, rocker.volume_extension] + extensions_modules
 
-
-# 2. Get Extensions Classes
-print("##### DETECTING EXTENSIONS #####")
-extensions_dict = {}
-for extension_mod in extension_modules:
-  for name, cls in inspect.getmembers(extension_mod):
-    if inspect.isclass(cls) and [b.__name__ for b in cls.__bases__][0] == 'RockerExtension':
-      print("Extension Class: ", name, " Inherits: ", [b.__name__ for b in cls.__bases__])
-      extensions_dict[name] = cls()
-    #end if
+  # 2. Get Extensions Classes
+  print("##### DETECTING EXTENSIONS #####")
+  extensions_dict = {}
+  for extension_mod in extension_mods:
+    for name, cls in inspect.getmembers(extension_mod):
+      if inspect.isclass(cls) and [b.__name__ for b in cls.__bases__][0] == 'RockerExtension':
+        print("Extension Class: ", name, " Inherits: ", [b.__name__ for b in cls.__bases__])
+        extensions_dict[name] = cls()
+      #end if
+    #end for
   #end for
-#end for
 
-# TODO: 3. Filter Inactive or Blacklisted Extensions
+  # TODO: 3. Filter Inactive or Blacklisted Extensions
 
-def generate_dockerfile(extensions, args_dict, base_image):
-    extensions = sort_extensions(extensions_dict, cli_args=args_dict)
+  # 4. Sort Extensions
+  return sort_extensions(extensions_dict, cli_args=args_dict)
+#end def
 
-    dockerfile_str = ''
-    # Preamble snippets
-    for el in extensions:
-        dockerfile_str += '# Preamble from extension [%s]\n' % el.get_name()
-        dockerfile_str += el.get_preamble(args_dict) + '\n'
-    dockerfile_str += '\nFROM %s\n' % base_image
-    # ROOT snippets
-    dockerfile_str += 'USER root\n'
-    for el in extensions:
-        dockerfile_str += '# Snippet from extension [%s]\n' % el.get_name()
-        dockerfile_str += el.get_snippet(args_dict) + '\n'
-    # Set USER if user extension activated
-    if 'user' in args_dict and args_dict['user']:
-        if 'user_override_name' in args_dict and args_dict['user_override_name']:
-            username = args_dict['user_override_name']
-        else:
-            username = "flatboat"
-        dockerfile_str += f'USER {username}\n'
-    # USER snippets
-    for el in extensions:
-        dockerfile_str += '# User Snippet from extension [%s]\n' % el.get_name()
-        dockerfile_str += el.get_user_snippet(args_dict) + '\n'
-    return dockerfile_str
+def generate_dockerfile(extensions_modules: list, args_dict: dict, base_image: str):
+  # 1. Get User Selected Extensions
+  extensions = get_extensions(extensions_modules, args_dict)
+
+  dockerfile_str = ''
+  # 2. Generate Preamble snippets
+  for el in extensions:
+    dockerfile_str += '# Preamble from extension [%s]\n' % el.get_name()
+    dockerfile_str += el.get_preamble(args_dict) + '\n'
+  #end for
+
+  # 3. Start from base_image
+  dockerfile_str += '\nFROM %s\n' % base_image
+
+  # 4. Generate ROOT snippets
+  dockerfile_str += 'USER root\n'
+  for el in extensions:
+    dockerfile_str += '# Snippet from extension [%s]\n' % el.get_name()
+    dockerfile_str += el.get_snippet(args_dict) + '\n'
+  #end for
+
+
+  # 5. Set USER if user extension activated
+  if 'user' in args_dict and args_dict['user']:
+    if 'user_override_name' in args_dict and args_dict['user_override_name']:
+      username = args_dict['user_override_name']
+    else:
+      username = "flatboat"
+    #end if
+    dockerfile_str += f'USER {username}\n'
+  #end if
+
+  # 6. Generate USER snippets
+  for el in extensions:
+      dockerfile_str += '# User Snippet from extension [%s]\n' % el.get_name()
+      dockerfile_str += el.get_user_snippet(args_dict) + '\n'
+  return dockerfile_str
 
 def generate_parameters():
   pass
