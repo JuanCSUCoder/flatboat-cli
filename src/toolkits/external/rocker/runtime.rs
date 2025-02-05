d822ca0eeb02b995cbfb03641dfc537e122cb03f
JuanCSUCoder <jcsu2019369@gmail.com>
2025-02-05 15:38:04 -0500

    Feat: Reorder code

--- src/toolkits/external/rocker/runtime.rs
+++ src/toolkits/external/rocker/runtime.rs
@@ -2,9 +2,9 @@ use std::{any::Any, collections::HashMap, ffi::CStr};
 
 use pyo3_ffi::c_str;
 use thiserror::Error;
-use pyo3::{types::PyModule, Python, PyErr};
+use pyo3::{types::{PyAnyMethods, PyModule, PyTuple}, Py, PyAny, PyErr, Python};
 
-const ROCKER_INTERFACE_SRC: &CStr = c_str!(include_str!("./rocker-interface.py"));
+const ROCKER_INTERFACE_SRC: &CStr = c_str!(include_str!("./rocker_interface.py"));
 
 #[derive(Error, Debug)] 
 enum RockerSetupError {
@@ -15,12 +15,18 @@ enum RockerSetupError {
 /// Setups and mantains the required environment for running a Rocker container
 pub async fn setup_environment(extension_modules: Vec<String>, arguments: HashMap<String, Box<dyn Any>>) -> Result<(), RockerSetupError> {
   Python::with_gil(|py| {
-    let function = PyModule::from_code(
+    let function: Py<PyAny> = PyModule::from_code(
       py,
       ROCKER_INTERFACE_SRC,
-      c_str!("rocker-interface.py"),
+      c_str!("rocker_interface.py"),
       c_str!("rocker_interface")
-    )?;
+    )?
+    .getattr("setup_environment")?
+    .into();
+
+    let args = (extension_modules, arguments);
+
+    function.call1(py, args)?;
 
     Ok(())
   })
